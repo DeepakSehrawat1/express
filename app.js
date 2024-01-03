44ad8d73b23372fd34afea22b1fa8b3b877c6bc5
DeepakSehrawat1 <deepaksehrawat150@gmail.com>
2024-01-03 22:34:46 +0530

    chap app

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express"); //return function
 const parser = require("body-parser"); //return obj
+const cookieParser = require("cookie-parser");
 
 const adminroutes = require("./routes/admin");
-const shoproutes = require("./routes/shop");
 
 const app = express();
 
 app.use(parser.urlencoded({ extended: false })); // here parser.urlencoded() returns a middleware that parses the body of incoming request
 // and add the data recieved to req.body and calls next function
+app.use(cookieParser());
 
 app.use(adminroutes);
-app.use(shoproutes);
 
 app.listen(5000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,51 @@
 const express = require("express");
+const fs = require("fs");
+const cookieParser = require("cookie-parser");
 
 const route = express.Router();
 
-route.post("/add-product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
+route.get("/", (req, res, next) => {
+  res.send(
+    `<h1>no text</h1><br><form action = "/" method = "POST"><input type = "text" name = "message"><button type="submit">send</button></form>`
+  );
 });
 
-route.get("/product", (req, res, next) => {
+route.post("/", (req, res, next) => {
+  const username = req.cookies?.username;
+  const message = req.body.message;
+  console.log("okk");
+
+  if (username && message) {
+    const data = `${username}: ${message}\n`;
+    fs.appendFile("message.txt", data, (err, data) => {
+      if (err) throw err;
+      fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+        if (err) throw err;
+        res.send(
+          `<h1>${data}</h1><br><form action = "/" method = "POST"><input type = "text" name = "message"><button type="submit">send</button></form>`
+        );
+      });
+    });
+  } else {
+    res.send("error");
+  }
+});
+
+route.post("/login", (req, res, next) => {
+  const username = req.body["username"];
+  if (username) {
+    res.cookie("username", username);
+    res.redirect("/");
+  } else {
+    res.send("Invalid username");
+  }
+});
+
+route.get("/login", (req, res, next) => {
   console.log("middleware 2");
+
   res.send(
-    `<form action = "/add-product" method = "POST"><input type = "text" name = "title"><button type="submit">send</button></form>`
+    `<form action = "/login" method = "POST"><input type = "text" name = "username"><button type="submit">enter</button></form>`
   );
 });
 
